bcc85ed6bb78456cf878f5a491241a7fc64fb87c
CatoyNan <xjn17858906105@gmail.com>
2018-08-10 08:08:34 +0800

    修复bug:图书图片无法写入服务器磁盘

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
